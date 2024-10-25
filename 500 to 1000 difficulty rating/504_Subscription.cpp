c0fed96a12550958924a95f564ef0a0cbe690fdf
Vikash Kumar <rajvikash18113@gmail.com>
2024-10-25 14:34:07 +0530

    Create 504_Subscription.cpp

---
+++ 500 to 1000 difficulty rating/504_Subscription.cpp
@@ -0,0 +1,28 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/SUBSCRIBE_
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t,n,x;
+    cin >> t;
+
+    while(t--)
+    {
+        cin >> n >> x;
+        if(n % 6 == 0)
+        {
+            int subs = n / 6;
+            cout << subs * x << endl;
+        }
+        else if (n % 6 != 0)
+        {
+            int subs = n / 6;
+            subs += 1;
+            cout << subs * x << endl;
+        }
+        
+        
+    }
+}
