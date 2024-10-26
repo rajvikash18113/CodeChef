d58c4c0f66fbd04405c958762b60b9dc9adf130e
Vikash Kumar <rajvikash18113@gmail.com>
2024-10-26 11:01:38 +0530

    Create 513_Janmansh_and_Assignment.cpp

---
+++ 500 to 1000 difficulty rating/513_Janmansh_and_Assignment.cpp
@@ -0,0 +1,24 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/JASSIGNMENTS
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x;
+	cin >> t;
+	
+	while(t--)
+	{
+	    cin >> x;
+	    if(x <= 7)
+	    {
+	        cout << "Yes" << endl;
+	    }
+	    else
+	    {
+	        cout << "No" << endl;
+	    }
+	}
+
+}
