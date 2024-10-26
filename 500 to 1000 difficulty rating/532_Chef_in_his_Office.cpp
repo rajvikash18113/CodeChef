a8c017a821f9174e7137cb2d79f260aea1077dbf
Vikash Kumar <rajvikash18113@gmail.com>
2024-10-26 11:01:43 +0530

    Create 532_Chef_in_his_Office.cpp

---
+++ 500 to 1000 difficulty rating/532_Chef_in_his_Office.cpp
@@ -0,0 +1,16 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/OFFICE
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t, x, y;
+	cin >> t;
+	
+	while(t--)
+	{
+	    cin >> x >> y;
+	    cout << (x*4) + y << endl;
+	}
+
+}
