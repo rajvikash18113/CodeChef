9a6e21b57766be555f5899c9183120ff771fdaa8
Vikash Kumar <rajvikash18113@gmail.com>
2024-11-01 08:43:59 +0530

    Create 563_Count_tthe_Notebook.cpp

---
+++ 500 to 1000 difficulty rating/563_Count_tthe_Notebook.cpp
@@ -0,0 +1,15 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/NOTEBOOK
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        cout << n *(1000/100) << endl;
+    }
+}
