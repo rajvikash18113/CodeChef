b77c4408a4553eab70276383c2ce425cd34916fb
Vikash Kumar <rajvikash18113@gmail.com>
2024-11-08 18:20:51 +0530

    Create 628_Watching_Movie_at_2x.cpp

---
+++ 500 to 1000 difficulty rating/628_Watching_Movie_at_2x.cpp
@@ -0,0 +1,11 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/MOVIE2X
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x, y;
+    cin >> x >> y;
+    cout << x - (y/2) ;
+    return 0;
+}
