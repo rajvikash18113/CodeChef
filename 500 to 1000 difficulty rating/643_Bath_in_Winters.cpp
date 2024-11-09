46d19e8bd141eb15530924ce126325e895b6a4c2
Vikash Kumar <rajvikash18113@gmail.com>
2024-11-09 13:24:11 +0530

    Create 643_Bath_in_Winters.cpp

---
+++ 500 to 1000 difficulty rating/643_Bath_in_Winters.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/BATH
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int X, Y;
+	    cin >> X >> Y;
+	    cout << X / (2*Y) << endl;
+	}
+	return 0;
+
+}
