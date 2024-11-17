850d34a7a26efe874b7f693adc7e56cfa3a49b7f
Vikash Kumar <rajvikash18113@gmail.com>
2024-11-17 10:39:25 +0530

    Create 660_Complementary_Strand _n_a_DNA.cpp

---
+++ 500 to 1000 difficulty rating/660_Complementary_Strand _n_a_DNA.cpp
@@ -0,0 +1,36 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/DNASTRAND?tab=statement
+
+#include<iostream>
+
+#include<string>
+
+using namespace std;
+
+char getcomplement(char c)
+{
+    if (c == 'A') return 'T';
+    else if (c == 'T') return 'A';
+    else if (c == 'C') return 'G';
+    else if (c == 'G') return 'C';
+    return 0;
+}
+
+int main() {
+    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        string s, result = "";
+        cin >> n >> s;
+        for (char c: s)
+        {
+            result += getcomplement(c);
+        }
+        cout << result << endl;
+    }
+
+    return 0;
+
+}
