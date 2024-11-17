f8c1365f0d6df78c1dcf86846133ea046874c6f4
Vikash Kumar <rajvikash18113@gmail.com>
2024-11-17 10:48:14 +0530

    Create 662_Chef_and_Water_Bottle.cpp

---
+++ 500 to 1000 difficulty rating/662_Chef_and_Water_Bottle.cpp
@@ -0,0 +1,26 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/CHEFBOTTLE
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x, k;
+        cin >> n >> x >> k;
+        if ((n * x) < k)
+        {
+            cout << n << endl;
+        }
+        else if ((n * x) > k)
+        {
+            cout << (k / x) << endl;
+        }
+    }
+    return 0;
+}
