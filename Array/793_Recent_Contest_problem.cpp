a9660a1c2e88b371e34fbd1c398a6ac793b78149
Vikash Kumar <rajvikash18113@gmail.com>
2024-11-25 16:26:44 +0530

    Create 793_Recent_Contest_problem.cpp

---
+++ Array/793_Recent_Contest_problem.cpp
@@ -0,0 +1,36 @@
+// https://www.codechef.com/problems/RECENTCONT?tab=statement
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        int result1 = 0, result2 = 0;
+        while (n--)
+        {
+            cin >> s;
+            
+           // if (s[0] == 'S')
+           if(s == "START38")
+            {
+                result1 += 1;
+            }
+            else
+            {
+                result2 += 1;
+            }
+
+        }
+        cout << result1 << " " << result2 << endl;
+    }
+    return 0;
+
+}
