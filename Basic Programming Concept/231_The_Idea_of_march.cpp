be204756996e2a612b96754b7a9839b0205eb748
Vikash Kumar <rajvikash18113@gmail.com>
2024-11-26 17:54:06 +0530

    Create 231_The_Idea_of_march.cpp

---
+++ Basic Programming Concept/231_The_Idea_of_march.cpp
@@ -0,0 +1,12 @@
+// https://www.codechef.com/problems/IDESM
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    cout << ((n == 15) ? "YES" : "NO") << endl;
+    return 0;
+}
