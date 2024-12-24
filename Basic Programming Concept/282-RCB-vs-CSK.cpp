d39a5521f2051f14cf0ba6f4c1603a45c4c7a026
Vikash Kumar <rajvikash18113@gmail.com>
2024-12-24 12:49:11 +0530

    Create 282-RCB-vs-CSK.cpp

---
+++ Basic Programming Concept/282-RCB-vs-CSK.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/problems/RCBCSK
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int x, y;
+	cin >> x >> y;
+	cout << ((x - y >= 18) ? "RCB" : "CSK") << endl;
+	return 0;
+
+}
