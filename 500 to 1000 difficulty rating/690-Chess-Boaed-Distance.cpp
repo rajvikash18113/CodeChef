bcb4eb3af4ace46ec5122d71c7d6b2c887e17f68
Vikash Kumar <rajvikash18113@gmail.com>
2024-12-24 14:54:21 +0530

    Update 690-Chess-Boaed-Distance.cpp

--- 500 to 1000 difficulty rating/690-Chess-Boaed-Distance.cpp
+++ 500 to 1000 difficulty rating/690-Chess-Boaed-Distance.cpp
@@ -3,17 +3,17 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 	int t;
 	cin >> t;
-	while(t--)
+	while (t--)
 	{
-	    int x1, y1, x2, y2;
-	    cin >> x1 >> y1 >> x2 >> y2;
-	    int dx = abs(x1 - x2);
-	    int dy = abs(y1 - y2);
-	    cout << max(dx, dy) << endl;
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+		int dx = abs(x1 - x2);
+		int dy = abs(y1 - y2);
+		cout << max(dx, dy) << endl;
 	}
 	return 0;
-
 }
