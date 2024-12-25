3aaa45a09d47b2d6e8dcf7fe27c13828f45024a5
Vikash Kumar <rajvikash18113@gmail.com>
2024-12-25 12:41:34 +0530

    CodeChef

---
+++ 500 to 1000 difficulty rating/691-Valentine-is-Coming.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/VALENTINE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int x, y;
+	    cin >> x >> y;
+	    cout << x / y << endl;
+	    
+	}
+    return 0;
+}
