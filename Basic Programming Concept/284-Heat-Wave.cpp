cd414eb36b2a0dc4f0daf3304dbcc8ea5ac4fe00
Vikash Kumar <rajvikash18113@gmail.com>
2024-12-27 21:39:27 +0530

    Create 284-Heat-Wave.cpp

---
+++ Basic Programming Concept/284-Heat-Wave.cpp
@@ -0,0 +1,12 @@
+// https://www.codechef.com/problems/HEATWAVE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x, y;
+	cin >> x >> y;
+	cout << ((x < y) ? "YES" : "NO") << endl;
+	return 0;
+
+}
