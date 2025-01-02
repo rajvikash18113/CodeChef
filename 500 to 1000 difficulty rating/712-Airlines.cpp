55843a8d4a21daefbeeb89ee94f73f3132445a24
Vikash Kumar <rajvikash18113@gmail.com>
2025-01-02 15:28:47 +0530

    Create 712-Airlines.cpp

---
+++ 500 to 1000 difficulty rating/712-Airlines.cpp
@@ -0,0 +1,26 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/SPCP2
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, n;
+        cin >> x >> n;
+        if (100 * x >= n)
+            cout << "0" << endl;
+        else
+        {
+            int rem = n - (100 * x);
+            if (rem % 100 == 0)
+                cout << rem / 100 << endl;
+            else
+                cout << (rem / 100) + 1 << endl;
+        }
+    }
+    return 0;
+}
