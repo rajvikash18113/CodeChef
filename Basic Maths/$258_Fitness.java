f1462fa437cbafd5fea67c312ba31c617ed57e05
Vikash Kumar <rajvikash18113@gmail.com>
2025-02-04 19:28:44 +0530

    Create $258_Fitness.java

---
+++ Basic Maths/$258_Fitness.java
@@ -0,0 +1,16 @@
+// https://www.codechef.com/practice/course/basic-math/BASICMATH/problems/FIT
+
+import java.util.Scanner;
+
+ class $258_Fitness {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        
+        while (t-- > 0) {
+            int x = scanner.nextInt();
+            System.out.println(x * 2 * 5);
+            
+        }
+    }
+}
