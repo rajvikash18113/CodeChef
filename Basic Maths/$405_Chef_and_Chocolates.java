0c1aea2455a64bd7b67fe55e7062d3d4f3a4c441
Vikash Kumar <rajvikash18113@gmail.com>
2025-02-06 22:37:39 +0530

    Create $405_Chef_and_Chocolates.java

---
+++ Basic Maths/$405_Chef_and_Chocolates.java
@@ -0,0 +1,18 @@
+// https://www.codechef.com/practice/course/basic-math/BASICMATH/problems/CCHOCOLATES
+
+import java.util.Scanner;
+
+class $405_Chef_and_Chocolates {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t--> 0) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            int z = scanner.nextInt();
+            System.out.println(((x * 5) + (y * 10)) / z);
+        }
+        scanner.close();
+    }
+}
